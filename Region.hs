98964f9a162a01aa7ba5a690ee9288855587f112
megajc <46426490+megajc@users.noreply.github.com>
2023-08-23 21:03:08 -0300

    available capacity for R
    done!

--- Region.hs
+++ Region.hs
@@ -24,10 +24,6 @@ linkR (Reg cities links tunnels) city1 city2 quality
  | notElem city1 cities || notElem city2 cities = error "At least one of the cities entered is not part of the region."
  | otherwise = Reg cities (newL city1 city2 quality:links) tunnels
 
-verificatesLinkRWorksCorrectly (Reg _ links _) = links
-
-
-
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
 tunelR (Reg cities links tunels) cityList
   | all (`elem` cities) cityList && allCitiesLinked = Reg cities links (newTunel:tunels)
@@ -36,9 +32,6 @@ tunelR (Reg cities links tunels) cityList
     allCitiesLinked = all (\(c1, c2) -> any (\l -> linksL c1 c2 l) links) (zip cityList (tail cityList))
     newLinks = [l | (c1, c2) <- zip cityList (tail cityList), l <- links, linksL c1 c2 l]
     newTunel = newT newLinks
-
-verificatesTunelRWorksCorrectly (Reg _ _ tunnel ) = tunnel
-
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 connectedR (Reg cities links tunels) city1 city2 =
@@ -54,9 +47,15 @@ delayR (Reg cities links tunels) city1 city2 = delayT (getConnectingTunel tunels
       getConnectingTunel (x:xs) city1 city2 | connectsT city1 city2 x = x
                                             | otherwise = getConnectingTunel xs city1 city2
 
-availableCapacityForR (Reg _ links _) city1 city2 = availableTunelsList links city1 city2
+availableCapacityForR :: Region -> City -> City -> Int
+availableCapacityForR (Reg cities links tunels) city1 city2 
+   | city1 `notElem` cities || city2 `notElem` cities = error "Those cities don't exist in this region" 
+   | otherwise = capacityL link - usedCapacity link tunels
    where
-   availableTunelsList links city1 city2 = foldr (\link -> (:).linkCount) 
-   linkCount links city1 city2 = foldr (\links fold -> if linksL city1 city2 links then fold + 1 else fold) 0
--- ir link por link viendo en cuanto tuneles esta, guardarlo en una lista
--- devolver el int mas chico de la lista
+      link = checkCitiesLink links city1 city2
+      usedCapacity link tunels = count True (map(usesT link)tunels)
+      count target = foldr (\link fold -> if target == link then fold +1 else fold) 0
+      checkCitiesLink links city1 city2 | null links = error "Link could not be found"
+                                        | linksL city1 city2 (head links) = head links
+                                        | otherwise = checkCitiesLink (tail links) city1 city2
+   
