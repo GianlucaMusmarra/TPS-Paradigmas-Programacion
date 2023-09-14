3d9ef9b352ca21e49af74afbbedc804fa191a445
GianlucaMusmarra <135612276+GianlucaMusmarra@users.noreply.github.com>
2023-09-14 10:59:27 -0300

    poniendo lindos los tests

--- TpJava1/src/QueueTest.java
+++ TpJava1/src/QueueTest.java
@@ -5,21 +5,25 @@ import static org.junit.Assert.*;
 
 public class QueueTest {
 
+  public static final String EXPECTED_ERROR_WAS_NOT_THROWN = "Expected Error was not thrown.";
+  public static final String QUEUE_IS_EMPTY = "Queue is empty";
+  public static final String SOMETHING = "Something";
+
   @Test
   public void test01QueueShouldBeEmptyWhenCreated() {
     assertTrue( new Queue().isEmpty() );
   }
 
   @Test public void test02AddElementsToTheQueue() {
-    assertFalse( new Queue().add( "Something" ).isEmpty() );
+    assertFalse( new Queue().add(SOMETHING).isEmpty() );
   }
 
   @Test public void test03AddedElementsIsAtHead() {
-    assertEquals( "Something", new Queue().add( "Something" ).head() );
+    assertEquals(SOMETHING, new Queue().add(SOMETHING).head() );
   }
 
   @Test public void test04TakeRemovesElementsFromTheQueue() {
-    Queue queue = new Queue().add( "Something" );
+    Queue queue = new Queue().add(SOMETHING);
     queue.take();
     
     assertTrue( queue.isEmpty() );
@@ -27,7 +31,7 @@ public class QueueTest {
 
   @Test public void test05TakeReturnsLastAddedObject() {
     Queue queue = new Queue();
-    String addedObject = "Something";
+    String addedObject = SOMETHING;
     queue.add( addedObject );
     
     assertEquals( addedObject, queue.take() );
@@ -58,7 +62,7 @@ public class QueueTest {
 
   @Test public void test08HeadDoesNotRemoveObjectFromQueue() {
     Queue queue = new Queue();
-    queue.add( "Something" );
+    queue.add(SOMETHING);
     assertEquals( 1, queue.size() );
     queue.head();
     assertEquals( 1, queue.size() );
@@ -72,31 +76,42 @@ public class QueueTest {
     Queue queue = new Queue();
     try {
       queue.take();
-      fail( "Expected Error was not thrown." );
+      fail(EXPECTED_ERROR_WAS_NOT_THROWN);
     } catch (Error e) {
-      assertTrue( e.getMessage().equals( "Queue is empty" ) );
+      assertTrue( e.getMessage().equals(QUEUE_IS_EMPTY) );
     }
   }
 
   @Test public void test09CanNotTakeWhenThereAreNoObjectsInTheQueueAndTheQueueHadObjects() {
     Queue queue = new Queue();
-    queue.add( "Something" );
+    queue.add(SOMETHING);
     queue.take();
     try {
       queue.take();
-      fail( "Expected Error was not thrown." );
+      fail(EXPECTED_ERROR_WAS_NOT_THROWN);
     } catch (Error e) {
-      assertTrue( e.getMessage().equals( "Queue is empty" ) );
+      assertTrue( e.getMessage().equals(QUEUE_IS_EMPTY) );
     }
   }
 
   @Test public void test10CanNotHeadWhenThereAreNoObjectsInTheQueue() {
     Queue queue = new Queue();
+
+
+
     try {
       queue.head();
-      fail( "Expected Error was not thrown." );
+      fail(EXPECTED_ERROR_WAS_NOT_THROWN);
     } catch (Error e) {
-      assertTrue( e.getMessage().equals( "Queue is empty" ) );
+      assertTrue( e.getMessage().equals(QUEUE_IS_EMPTY) );
     }
+  }
+
+  public void assetThrowsLike(){
+
+
+
+
+
   }
 }
